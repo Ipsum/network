7e13df0727c206a814ac66d26dc68f8fcb59a337
Ipsum <desertfox59@gmail.com>
2014-02-17 22:24:21 -0500

    Added GUI Please enter the commit message for your changes. Lines starting

--- lab2/ReadMe.txt
+++ lab2/ReadMe.txt
@@ -1,5 +1,4 @@
 ---- ReadMe.txt for UDP server and client ---
----- **** UPDATED FOR PHASE 2 : 2/16/14 **** ----
  - Authors: David Tyler and Andrew Hajj
  - Date:    2/2/14
  - Modules: Server.py, Client.py
@@ -24,7 +23,7 @@
  Client.py is the module run as the UDP client process.
     - To run Client.py, first make sure that the server on cato.ednos.net is up.
     - To change the host or port, edit HOST or PORT in Client.py 
-    - NOTE - any size file should be able to be sent over just fine as it breaks it up into 1 kb packets
+    - NOTE - best results when files are under 10kb
     - To send a file to the server:
         1. First add the desired file to the folder containing Client.py
         2. Run the Command prompt (on Windows)
@@ -39,3 +38,16 @@
             - Alternatively, 'g' can be used instead of 'g'
         5. The file should then be available in the directory
     
+2_ - To send a file to the server:
+        1. First add the desired file to the folder containing Client.py
+        2. Run the Command prompt (on Windows)
+        3. Navigate to the directory containing the file and Client.py
+        4. Run the script in the following manner - "python Client.py send -filename-"
+            - Alternatively, 's' can be used instead of 'send'
+    - To get a file from the server:
+        1. First add the desired file to the folder containing Client.py
+        2. Run the Command prompt (if in Windows)
+        3. Navigate to the directory containing the file and Client.py 
+        4. Run the script in the following manner - "python Client.py get -filename-"
+            - Alternatively, 'g' can be used instead of 'g'
+        5. The file should then be available in the directory
--- lab2/client.py
+++ lab2/client.py
@@ -8,8 +8,8 @@
 #           but holds little similarity now
 # Purpose: Client side for a UDP server in Python.  Can send and receive files
 #          (under 10kB works best, as found from tests)
-# Usage:    Run this file with arguments to send/get a file to/from the server
-#           ex. 'python Client.py send sample.jpg'
+# Usage:    Run this file and use gui to specify file to send/receive and the server details
+#           ex. 'python client.py'
 #
 # ******** UPDATED 2/13/14 ***********
 
@@ -21,118 +21,153 @@ import sys
 import time
 import os
 
+from Tkinter import *
+from ttk import *
+
 #HOST, PORT = "cato.ednos.net", 4422
 HOST, PORT = "localhost", 9999
 
 # Size of the packets to be sent
 buf = 1024
+class GUI:
+    def __init__(self,master):
+        """initial menu setup"""
+        self.master = master
+        #create menu
+        menubar = Menu(self.master)
+        self.master['menu'] = menubar
+        menu_file = Menu(menubar)
+        menu_file.add_command(label='Exit', command=self.exitcmd)
+        #communications settings
+        Label(master, text="Address").grid(row=0,column=0)
+        Label(master, text="Port").grid(row=1,column=0)
+        self.addr = StringVar()
+        Entry(master,textvariable=self.addr).grid(row=0,column=1)
+        self.port = StringVar()
+        Entry(master,textvariable=self.port).grid(row=1,column=1)
+        
+        self.addr.set(HOST)
+        self.port.set(str(PORT))
+        
+        #send file elements
+        self.fname = StringVar()
+        Label(master, text="File name").grid(row=2,column=1)
+        Entry(master,textvariable=self.fname).grid(row=2,column=0)
+        
+        Button(master,text="Send File",command=self.send).grid(row=3,column=0)
+        Button(master,text="Get File",command=self.get).grid(row=3,column=1)        
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect((HOST, PORT))
-sock.settimeout(10)
+    def send(self):
+        "Send a file" 
+        #socket stuff
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(10)
+        host = self.addr.get()
+        port = int(self.port.get())
+        #connect
+        self.sock.connect((host, port))
+        
+        print "Checking if {} exists".format(self.fname.get())
 
-# When running the file, it should be run as 'Client.py action fileName'
-# ex. Client.py send image.jpg
-
-# Get info on send or receive
-action = sys.argv[1]
-
-# Get the name of the file
-fileName = sys.argv[2]
-
-# Print the action and name of the file locally (to ensure it is correct)
-print "You chose to", action, fileName
-
-# Sending an image
-if action.lower() in ["s", "send"]:
-
-    print "Checking if {} exists".format(fileName)
-
-    #Check to make sure the file actually exists
-    check = os.path.isfile(fileName)
-    if check is True:
-        # open the file
-        file = open(fileName, "rb")
-        print "{} opened".format(fileName)
-        # Counter is sent along with the packet so that the server knows which number packet it is
-        counter = 1
-        # Fill the first packet
-        data = file.read(buf)
-        # Let the server know a new file is being sent along with the first packet
-        sock.send("new_" + fileName + " " + str(counter) + "_" + data)
-        sys.stdout.write('Sending...')
-        data = file.read(buf)
-        # Send the rest of the file over in packets
-        while(data):
-            # Sleep has been added in order to not over flow the buffer
-            time.sleep(.15)
-            counter = counter + 1
-            # Send file's name and the next packet
-            sock.send(fileName+" "+str(counter)+"_"+data)
-            sys.stdout.write('.')
-            # Get the next packet to be sent
+        #Check to make sure the file actually exists
+        check = os.path.isfile(self.fname.get())
+        if check is True:
+            # open the file
+            file = open(self.fname.get(), "rb")
+            print "{} opened".format(self.fname.get())
+            # Counter is sent along with the packet so that the server knows which number packet it is
+            counter = 1
+            # Fill the first packet
             data = file.read(buf)
-            # If there is no more data, break out of the loop
-            if data is 0:
-                break
-        # Close file after sending
-        file.close()
-        print "Done!"
-
-        print "Attempting to get a message back.."
-        try:
-            # Store returned message
-            received = sock.recv(1024)
-            # Print returned message
-            print "Received: {}".format(received)
-        except socket.timeout:
-            print "Could not get a return message."
-    else:
-        print "{} does not exist...please try again".format(fileName)
-
-
-elif action.lower() in ["g", "get"]:
-
-    print "Requesting", fileName+"..."
-
-    # Create a new file
-    try:
-        file = open(fileName, 'wb')
-    except:
-        print "There was a problem saving the file..."
-
-    # Send the name of the file to be pulled from the server
-    sock.send(fileName)
-
-    return_message = sock.recv(buf)
-    #print return_message
-    if "not found" in return_message:
-        print "{} not found".format(fileName)
-    else:
-        print "Creating {} from server...".format(return_message)
-    
-        # Now get and write the data (in 1kb packets)
-        data = sock.recv(buf + sys.getsizeof(fileName+" "))
-        sys.stdout.write('Receiving...')
-        try:
+            # Let the server know a new file is being sent along with the first packet
+            self.sock.send("new_" + self.fname.get() + " " + str(counter) + "_" + data)
+            sys.stdout.write('Sending...')
+            data = file.read(buf)
+            # Send the rest of the file over in packets
             while(data):
-                #Parse the packet
-                recievedFileName, sep, data = data.partition(" ")
+                # Sleep has been added in order to not over flow the buffer
+                time.sleep(.15)
+                counter = counter + 1
+                # Send file's name and the next packet
+                self.sock.send(self.fname.get()+" "+str(counter)+"_"+data)
                 sys.stdout.write('.')
-                # Write that packet to the file
-                file.write(data)
-                #Receive the next packet to be saved to the file
-                data = sock.recv(buf + sys.getsizeof(fileName+" "))
-        except socket.timeout:
+                # Get the next packet to be sent
+                data = file.read(buf)
+                # If there is no more data, break out of the loop
+                if data is 0:
+                    break
+            # Close file after sending
+            file.close()
             print "Done!"
-            print "***    Received file: {}   ***".format(recievedFileName)
 
-        file.close()
+            print "Attempting to get a message back.."
+            try:
+                # Store returned message
+                received = self.sock.recv(1024)
+                # Print returned message
+                print "Received: {}".format(received)
+            except socket.timeout:
+                print "Could not get a return message."
+        else:
+            print "{} does not exist...please try again".format(self.fname.get())
+            
+        self.sock.close()
 
-else:
-    print "Requested action, {}, is not recognised...".format(action)
-    print "Supported actions are: send [s or send] or get [g or get]"
+    def get(self):
+        "Get a file"
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(10)
+        host = self.addr.get()
+        port = int(self.port.get())
+        self.sock.connect((host, port))
+        
+        print "Requesting", self.fname.get()+"..."
+        # Create a new file
+        try:
+            file = open(self.fname.get(), 'wb')
+        except:
+            print "There was a problem saving the file..."
 
+        # Send the name of the file to be pulled from the server
+        self.sock.send(self.fname.get())
 
-# Close socket connection
-sock.close()
+        return_message = self.sock.recv(buf)
+        #print return_message
+        if "not found" in return_message:
+            print "{} not found".format(self.fname.get())
+        else:
+            print "Creating {} from server...".format(return_message)
+        
+            # Now get and write the data (in 1kb packets)
+            data = self.sock.recv(buf + sys.getsizeof(self.fname.get()+" "))
+            sys.stdout.write('Receiving...')
+            try:
+                while(data):
+                    #Parse the packet
+                    recievedFileName, sep, data = data.partition(" ")
+                    sys.stdout.write('.')
+                    # Write that packet to the file
+                    file.write(data)
+                    #Receive the next packet to be saved to the file
+                    data = self.sock.recv(buf + sys.getsizeof(self.fname.get()+" "))
+            except socket.timeout:
+                print "Done!"
+                print "***    Received file: {}   ***".format(recievedFileName)
+
+            file.close()
+        self.sock.close()
+        
+    def exitcmd(self):
+        "closes program"
+        os._exit(99) #unconditional shutdown signal
+        
+if __name__ == "__main__":
+    #setup gui stuff
+    root = Tk()
+    sty = Style()
+    sty.configure('.', font='helvetica 15')
+    sty.configure('Tab', font='helvetica 8 bold')
+    root.title("Lab 2")
+    root.option_add('*tearOff', FALSE)
+    GUI(root)
+    root.mainloop()
