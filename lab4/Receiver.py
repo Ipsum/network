8cc730d929602f0b0fa4b116056e8b4473bcecb9
Ipsum <desertfox59@gmail.com>
2014-04-06 16:42:04 -0400

    minor code clean up

--- lab4/Receiver.py
+++ lab4/Receiver.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-""" server.py
+""" Receiver.py
 
  A simple UDP server to receive and send files over the internet
 
  Usage:
   Run this file with arguments for the ip and self.socket to bind to.
 
-  python server.py cato.ednos.net 4422
+  python Receiver.py cato.ednos.net 4422
 
 
  Files are saved and served from same directory as this script.
@@ -30,9 +30,7 @@ __email__ = "dtyler@gmail.com"
 __status__ = "Development"
 # only change these if not run with commandline args
 _HOST = "localhost"
-#_HOST = "cato.ednos.net"
 _PORT = 9999
-#_PORT = 4422
 PKTNUMBR = 0
 filename = 0
 OptThree = "N"
@@ -96,9 +94,7 @@ class MyUDPHandler(SocketServer.BaseRequestHandler):
             else:
                 print "PKTNUMBR: "+str(PKTNUMBR)
                 self.ack(not PKTNUMBR)
-        #assume is requesting file
-        #else:
-        #    self.sendfile(filename)
+
     def ack(self,nbr):
         "send ack message"
         m = struct.pack("!?H",nbr,self.crc16(struct.pack("!?",nbr)))
@@ -159,33 +155,7 @@ class MyUDPHandler(SocketServer.BaseRequestHandler):
 
     def sendfile(self, filename):
         "This function responds to client with requested file"
-
-        try:
-            f = open(filename, 'rb')
-        except:
-            #self.socket.sendto("{} not found".format(filename),
-            #self.client_address)
-            print "can't find "+filename
-            return False
-
-        #succeeded in opening file, now send requested file to client 1kb
-        #chunks first indicate the start of a file with: "new filename"
-        #self.socket.sendto("new "+filename, self.client_address)
-        #spool out the data kb by kb
-        data = f.read(1024)
-        while data:
-            # wait before sending the next instruction in order to not
-            # overflow the buffer
-            time.sleep(.15)
-            output = filename+" "+data
-            # Send the parsed data
-            self.socket.sendto(output, self.client_address)
-            # Read in the next packet to be sent
-            data = f.read(1024)
-        f.close()
-
-        return True
-
+        pass
 
 if __name__ == "__main__":
     try:
