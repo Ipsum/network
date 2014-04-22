fb86001b58a6e0f44e7e0eb05eb45929dee8534f
Ipsum <desertfox59@gmail.com>
2014-04-21 20:31:54 -0400

    more receiver stuff

--- lab5/receiver.py
+++ lab5/receiver.py
@@ -32,10 +32,14 @@ __status__ = "Development"
 class MyUDPHandler(SocketServer.BaseRequestHandler):
     "UDP server class to handle incoming data and return response"
     
+    def configure(self,window):
+        "configure server before running"
+        self.maxwindow=window
+        
     def handle(self):
         "extract and read packet header"
         #determine if SYN, data, FIN or ACK
-        self.socket = self.request[1]
+        self.address = self.request[1]
         data=self.request[0]
         header,data=self.decode(data)
         if header[3]:
@@ -63,20 +67,78 @@ class MyUDPHandler(SocketServer.BaseRequestHandler):
         
         return (seq,acknbr,ACK,SYN,FIN,window),data
         
+    def header(self):
+        "build packet header"
+        self.len = 4<<4 #len of packet head in 32bit words, upper half of byte
+        flags = 0
+        if self.state==1:
+            flags |= 1<<1
+        if self.ack:
+            flags |= 1<<4
+        if self.state==3:
+            flags |= 1
+        pkt=struct.pack("!IIBBH",self.seq,self.acknbr,self.len,flags,self.window)
+        checksum = crc16(pkt)
+        pkt=struct.pack("!IIBBHHxx",self.seq,self.acknbr,self.len,flags,self.window,checksum)
+        
+        return pkt        
+        
     def syn(self,header):
         #generate a random seq nbr
+        self.seq = random.randint(0,4294967295)
         #respond with SYN=1, random seq nbr, ack=incoming seq+1,windowsize
+        self.acknbr=header[0]+1
+        self.state=1
+        self.window=self.maxwindow
+        self.ack=0
+        header = self.header()
+        socket.sendto(header,self.address)
         #set state indicating that we still need an ack in the save function
+        self.state=1
         #create new file
+        self.createfile("received.jpg","")
+        
     def fin(self,header):
         #check that connection is in an open state
-        #send an ACK
-        #send a FIN and get ACK - close connection
+        if self.state==3:
+            #check for ACK and close connection
+            if header[
+        if self.state==2:
+            #send an ACK
+            self.ack=1
+            pkt=self.header()
+            socket.sendto(pkt,self.address)
+            #send a FIN
+            self.state=3
+            self.ack=0
+            pkt=self.header()
+            socket.sendto(pkt,self.address)
+        else:
+            return
+            
     def save(self,header,data):
         #check state - if in state one, move to state 2 on ACK otherwise nothing
+        if self.state==1:
+            if header[2]:
+                self.state=2
+            return
+            
+        if self.state==3:
+            if header[2]:
+                #close connection
+                self.state=1
+            return
         #if in state 2, process data
+        if self.state != 2:
+            return
         #check seq number = current ack value otherwise resend current ack value
-        #adjust window size
+        if not header[0] = self.acknbr:
+            self.ack=1
+            #save packet? and modify window size
+            pkt = self.header()
+            socket.sendto(pkt,self.address)
+            return
+        #check for saved packets and adjust window size
         #save data by append to file made in syn
         #send ack with received seq+bytesrecv+1 as acknbr,incoming ack as seq nbr
     def savefile(self, filename, data):
